bda43d7f6b6c5ca098e8dbadf466a1d6233a0943
Foday027 <159056257+Foday027@users.noreply.github.com>
2024-06-20 18:20:03 +0000

    TodlistWithReactAndFetch

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -1,52 +1,55 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
 import axios from 'axios';
+import './App.css'
 
-const TodoList = () => {
+function App() {
 
-    const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState('');
 
-    // const handleClickDelete = () => {
+  // const handleClickDelete = () => {
 
-    // }
-
-
-    useEffect(() => {
-        async function fetchData() {
-            let data = await axios.get('https://playground.4geeks.com/todo/users/saeedkallon');
-            let respData = await data.data;
-            setName(respData);
-            console.log(respData)
-        }
-        fetchData();
-    }, []);
-
-
-
-    
-    const createData = axios.post('https://playground.4geeks.com/todo/users/saeedkallon', {
-        NickName: 'Bones',
-        Hobby: 'Football',
-        Aim: 'Web Developer',
-        ShoppingCart: 'Mechanical Keyboard + LG Monitor',
-        Order: [1, 2, 3]
-    }, {
-        headers: { "content-type": "application/json" },
-       
-       }
-    )
-    console.log(createData)
+  // }
 
 
 
 
+  useEffect(() => {
+    async function fetchData() {
+      let resp = await axios.get('https://playground.4geeks.com/todo/users/saeed');
+      let respData = await resp.data.todos;
+      setTodos(respData);
+      console.log(todos)
+    }
+    fetchData();
+  }, []);
 
 
+  // const createData = axios.post('https://playground.4geeks.com/todo/users/saeedkallon', {
+  //     NickName: 'Bones',
+  //     Hobby: 'Football',
+  //     Aim: 'Web Developer',
+  //     ShoppingCart: 'Mechanical Keyboard + LG Monitor',
+  //     Order: [1, 2, 3]
+  // }, {
+  //     headers: { "content-type": "application/json" },
 
-    return (
-        <div className='text-center'>
-            hello
-        </div>
-    );
-};
+  //    }
+  // )
+  // console.log(createData)
 
-export default TodoList;
+  return (
+    <div className='text-center text-dark'>
+      <form className='form-container border border-dark'>
+        <label className='label me-1'>My must buy:</label>
+        <input className='bg-success rounded'/>
+      </form>
+
+      
+      {/* {todos.map((task, i) => (
+
+      ))} */}
+    </div>
+  );
+}
+
+export default App;
