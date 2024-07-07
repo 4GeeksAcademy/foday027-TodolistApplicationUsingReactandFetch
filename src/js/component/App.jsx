d77b9f11bf036882d1b8efb9e39a5532505fcc57
Foday027 <159056257+Foday027@users.noreply.github.com>
2024-07-07 18:51:07 +0000

    TodolistApplicationUsingRaactandFetch

--- src/js/component/App.jsx
+++ src/js/component/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import React from 'react';
 
 function App() {
 
@@ -12,25 +13,39 @@ function App() {
     setInput('')
   };
 
-  // const handleDelete = (i) => {
+  // const handleDelete = (i) => { 
   //   let aux = todos;
   //   aux = aux.filter((el, index) => i !== index);
   //   setTodos(aux);
   // }
 
 
+  async function fetchData() {
+    let resp = await axios.get('https://playground.4geeks.com/todo/users/saeed');
+    let respData = await resp.data;
+    setTodos(respData);
+    console.log(todos)
+  }
+
+  const handleDelete = (i) => {
+    let aux = todos;
+    aux = aux.filter((el, index) => i !== index);
+    setTodos(aux);
+  };
+
+
+  const handleEdit = (i) => {
+    let aux = todos;
+    aux = aux.filter((el, index) => i == !index);
+  };
 
 
   useEffect(() => {
-    async function fetchData() {
-      let resp = await axios.get('https://playground.4geeks.com/todo/users/saeed');
-      let respData = await resp.data;
-      setTodos(respData);
-      console.log(todos)
-    }
     fetchData();
+    handleDelete()
 
   }, []);
+
 
 
   const createPost = async (text) => {
@@ -51,13 +66,13 @@ function App() {
 
 
   return (
-    <div className='text-center text-light bg-secondary pb-1 rounded'>
+    <div className='main text-center text-light mt-5 pb-1 rounded'>
 
-      <form className='mx-3' onSubmit={handleSubmit}>
+      <form className=' form-control form mx-3' onSubmit={handleSubmit}>
 
-        <label className='label me-1 mb-2 text-primary'><h1><em>My must have</em></h1></label><br />
+        <label className='label me-1 mb-3 mb-2 text-dark'><h1><em>My must have!</em></h1></label><br />
         <input
-          className="bg-success rounded  me-2"
+          className=" rounded  me-2"
           type='text'
           placeholder='Add to your desire'
           value={input}
@@ -65,17 +80,20 @@ function App() {
             setInput(e.target.value)
           }}
         />
-        <button className='mb-3' type='submit'>Submit</button>
+        <button className='mb-3 bg-dark rounded button' type='submit'>Submit</button>
+
+        <ul>
+          {todos && todos.map((el, i) => {
+            return (
+              <li className=" m list list-style list-group-item mb-3 mt-1 ms-4 rounded " key={i}>{el.label}
+                <span className='mx-3' onClick={() => handleDelete(i)}><i className=" fa-solid fa-beat fa-trash trash"></i></span>
+                <span onClick={() => handleEdit(i)}><i className=" fa-solid fa-beat fa-edit edit"></i></span>
+              </li>
+
+            )
+          })}
+        </ul>
       </form>
-      <ul>
-        {todos && todos.map((el, i) => {
-          return (
-
-            <li className=" list-style list-group-item border-light mt-1 me-4 rounded" key={i}>{el.label}</li>
-
-          )
-        })}
-      </ul>
 
     </div>
   );
